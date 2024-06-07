0bf28ca9ab0be63dfe338f6b3a6e352e1309769d
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-06-06 22:27:03 -0600

    Create caracteresEscape.py

---
+++ 1_Iniciando_Python/caracteresEscape.py
@@ -0,0 +1,8 @@
+#Los caracteres de escape estan dados por \n
+
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+# \ Significa caracter de escape, es decir, la cadena de caracteres escapa por un tiempo muy corto
+# n tiene  significado de newline
