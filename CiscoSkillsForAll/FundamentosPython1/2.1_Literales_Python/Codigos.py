82c49147bec0a7839546d26a5a0738baddecd293
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-03 13:42:40 -0600

    Update and rename Enteros.py to Codigos.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/Codigos.py
@@ -0,0 +1,31 @@
+//Imprime (lo mismo) pero loalmacena de forma distinta 
+print("2")
+print(2)
+//Numero Octal
+print(0o123)
+//Numero Hexadecimal
+print(0x123)
+
+//Flotantes
+print(3E8)
+
+//Cadenas
+  //Para evitar errores, con caracter de escape tratando de colocar comillas dobles
+print("Me gusta \"Monty Python\"")
+  //O con apostrofes
+print('Me gusta "Monty Python"')
+//Ahora tratando de colocar apostrofes
+  print('I\'m Monty Python.')
+
+//Valores Booleanos
+
+print(True > False)
+print(True < False)
+
+//Literales de Python
+  //Uso de caracteres de escape
+print('"Estoy"\n""aprendiendo""\n"""Python"""')
+print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")
+ 
+
+
