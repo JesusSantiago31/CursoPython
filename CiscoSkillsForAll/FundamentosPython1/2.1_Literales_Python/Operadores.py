073b56ff91bfeee348b8a82f3ef20effbac1ac85
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-03 15:29:43 -0600

    Create Operadores.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/Operadores.py
@@ -0,0 +1,40 @@
+//  Operadores en Python + - * / // % **
+  //Multiplicacion
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+  //DIvision Flotante
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+  //Division Entera (Redondea el resultado)
+print(6 // 3) //Arroja un entero
+print(6 // 3.) //Arroja un flotante 
+print(6. // 3)
+print(6. // 3.)
+
+  
+print(6 // 4)  //Arroja un entero
+print(6. // 4) //Arroja un flotante 
+
+print(-6 // 4)
+print(6. // -4)
+
+//Residuo 
+print(14 % 4)
+
+//Suma
+print(-4 + 4)
+print(-4. + 8)
+
+//Resta
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+
+
+
+      
