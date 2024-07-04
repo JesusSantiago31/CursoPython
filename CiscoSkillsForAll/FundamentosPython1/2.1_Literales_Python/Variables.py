5515043bfe4072a7f0a082d706e0f78e9e3d06d6
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-04 09:22:05 -0600

    Create Variables.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/Variables.py
@@ -0,0 +1,26 @@
+var = 1
+print(var)
+
+//Se pueden usar las variables que sean necesarias para lograr el objetivo del progrma
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+//Se pueden combinar String's con varibales
+ var = "3.8.5"
+print("Python version: " + var) 
+
+//Se puede reasignar valores a las variables
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+ var = 100
+var = 200 + 300
+print(var) 
+
+  //Resolviendo el teorema de Pitagoras
+
