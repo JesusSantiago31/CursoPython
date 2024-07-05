c38217254e9399cba73805e4b9bbb77beb11089a
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-04 21:27:36 -0600

    Create UsoOperadores.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/UsoOperadores.py
@@ -0,0 +1,22 @@
+//Teorema de Pitagoras
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+//Operacion basica
+john = 3
+mary = 5
+adam = 6
+print(john , mary , adam, sep=" ,")
+ 
+total_apples= john + mary + adam
+
+print("Total de Manzanas: ", total_apples)
+
+//Abreviacion de OPeraciones
+x = x * 2
+sheep = sheep + 1
+
+ x *= 2
+sheep += 1 
