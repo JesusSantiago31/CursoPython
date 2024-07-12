c7cf0354aa53507409fb7e68553809aedd50f0ba
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-11 22:25:49 -0600

    Create OperacionesBasicasInput.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/OperacionesBasicasInput.py
@@ -0,0 +1,19 @@
+# ingresa un valor flotante para la variable a aquí
+a=float(input("Ingresa el valor de la variable a: "))
+# ingresa un valor flotante para la variable b aquí
+b=float(input("Ingresa el valor de la variable b: "))
+
+# mostrar el resultado de la suma aquí
+res= a + b
+print(a , " + " , b , " = " , res)
+# mostrar el resultado de la resta aquí
+res= a - b
+print(a , " - " , b , " = " , res)
+# mostrar el resultado de la multiplicación aquí
+res= a * b
+print(a , " * " , b , " = " , res)
+# mostrar el resultado de la división aquí
+res= a / b
+print(a , " / " , b , " = " , res)
+
+print("\n¡Eso es todo, amigos!")
