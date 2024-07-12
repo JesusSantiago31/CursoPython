4f9fe8b025a2bb75e1dc8a928dca7ed2a89f83e1
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-07-11 22:05:27 -0600

    Create ConvertirTipos.py

---
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/ConvertirTipos.py
@@ -0,0 +1,8 @@
+#Basic
+anything = input("Ingresa un número: ")
+something = anything ** 2.0
+print(anything, "al cuadrado es", something)
+#Converted
+anything = float(input("Ingresa un número: "))
+something = anything ** 2.0
+print(anything, "a la potencia de 2 es", something)
