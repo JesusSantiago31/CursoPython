42d2561b5ce6803a07f19835d4353b3e4e31e714
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-08-07 21:47:24 -0600

    Update and rename OperadorIgualdad.py to Operadores.py

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/Operadores.py
@@ -0,0 +1,17 @@
+#Igualdad ==
+var = 0  # Asignando 0 a var
+print(var == 0)
+
+var = 1  # Asignando 1 a var
+print(var == 0)
+
+#True
+#False
+
+#Desigualdad !=
+var = 0  # Asignando 0 a var
+print(var != 0)
+
+var = 1  # Asignando 1 a var
+print(var != 0)
+
