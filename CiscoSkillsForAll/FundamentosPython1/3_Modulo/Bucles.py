8da15a7dee4633addbc5f1df1aa00803db3d0470
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-08-29 09:04:17 -0600

    Funcionamiento de los bucles

--- CiscoSkillsForAll/FundamentosPython1/3_Modulo/Bucles.py
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/Bucles.py
@@ -6,3 +6,10 @@ while conditional==True:
 while True:
     print("Estoy atrapado dentro de un bucle.")
 
+#Ejemplo para salir de un bucle
+counter = 5
+while counter != 0:
+    print("Dentro del bucle.", counter)
+    counter -= 1
+print("Fuera del bucle.", counter)
+
