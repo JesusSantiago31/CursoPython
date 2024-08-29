14d0f17b44811b2cb55f5bae052570d4130c7d5f
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-08-29 09:31:26 -0600

    Contador usando for

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/ContadorFor.py
@@ -0,0 +1,12 @@
+# Escribe un bucle for que cuente hasta cinco.
+    # Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    # Cuerpo del bucle, emplea : time.sleep(1)
+
+# Escribe una función print con el mensaje final
+import time
+
+for i in range(1,6):
+    print(i, " Mississippi.")
+    time.sleep(1)
+    
+print("Lista o no, aquí vengo!")
