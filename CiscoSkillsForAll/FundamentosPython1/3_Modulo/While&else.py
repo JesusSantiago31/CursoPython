7f5415dfb4efa9dbcf948d2944a33cf67f948e44
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-02 22:03:32 -0600

    Uso de else

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/While&else.py
@@ -0,0 +1,15 @@
+#La rama else, siempre se ejecuta al menos una sola vez en bucle while
+#Se haya entrado en el cuerpo del bucle o no
+i = 1
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
+  
+  i = 5
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("else:", i)
