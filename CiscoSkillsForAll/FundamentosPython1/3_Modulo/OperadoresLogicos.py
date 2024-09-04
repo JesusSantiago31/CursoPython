aedf72360fe2f255817e9994217d9c87f86103bd
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-03 22:38:01 -0600

    Operadores Logicos

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/OperadoresLogicos.py
@@ -0,0 +1,23 @@
+#Operador and 
+#Prioridad baja que los operadores de comparacion
+counter > 0 and value == 100
+#Se basa en las tablas de verdad
+# TRUE & TRUE = TRUE
+# TRUE & FALSE = FALSE
+# FALSE & TRUE = FALSE
+# FALSE & FALSE = FALSE
+
+#Operador or
+#Prioridad mas baja que and
+
+#Tabla de verdad
+# TRUE | TRUE = TRUE
+# TRUE | FALSE  = TRUE
+# FALSE | TRUE = TRUE
+# FALSE | FALSE = FALSE
+
+#Operador not
+#Tabla de verdad
+# TRUE = FALSE
+#FALSE = TRUE
+
