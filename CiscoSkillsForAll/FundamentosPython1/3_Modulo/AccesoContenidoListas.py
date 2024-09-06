fd09d87661f1b27ad0ffe7d937781f5202fc1fa9
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-05 22:12:44 -0600

    Create AccesoContenidoListas.py

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/AccesoContenidoListas.py
@@ -0,0 +1,15 @@
+#print(numbers[0]) # Accediendo al primer elemento de la lista.
+numbers = [10, 5, 7, 2, 1]
+print("Contenido de la lista original:", numbers)  # Imprimiendo el contenido de la lista original.
+
+numbers[0] = 111
+print("\nContenido de la lista con cambio:", numbers)  # Imprimiendo contenido de la lista con 111.
+
+numbers[1] = numbers[4]  # Copiando el valor del quinto elemento al segundo elemento.
+print("Contenido de la lista con intercambio:", numbers)  # Imprimiendo contenido de la lista con intercambio.
+
+print("\nLongitud de la lista:", len(numbers))  # Imprimiendo la longitud de la lista.
+
+print(numbers)  # Imprimiendo la lista completa.
+
+
