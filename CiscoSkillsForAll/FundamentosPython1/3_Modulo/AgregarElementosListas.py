3087159b5beec99b18f99b658faa120b5f5fed39
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-09 23:17:10 -0600

    Create AgregarElementosListas.py

---
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/AgregarElementosListas.py
@@ -0,0 +1,22 @@
+# Agregar elementos a una lista 
+# Metodo append(): Agrega el elemento al final de la lista
+# Metodo insert(): Agrega el elemento en cualquier lugar de la lista
+
+numbers = [111, 7, 2, 1]
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.append(4)
+
+print(len(numbers))
+print(numbers)
+
+###
+
+numbers.insert(0, 222)
+print(len(numbers))
+print(numbers)
+
+#
