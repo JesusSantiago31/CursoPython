a3b4ab508c702b8a6a675389d0a03e17b63cad98
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-10 23:39:51 -0600

    Create DireccionesCorreo.py

---
+++ EjerciciosPracticos/DireccionesCorreo.py
@@ -0,0 +1,9 @@
+"""
+GENERAR X CANTIDAD DE CORREOS NUMERADOS CON TU NOMBRE
+"""
+nombre = input("Introduce tu nombre: ")
+cantidad = int(input("Cuantos correos electronicos necesitas:" ))
+
+for i in range(cantidad):
+      print(f"{nombre}{i}@gmail.com")
+                     
