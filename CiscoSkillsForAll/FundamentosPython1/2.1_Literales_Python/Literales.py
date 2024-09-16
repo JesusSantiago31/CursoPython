ececddfc95379ee786b3651d431c798e013dfa0c
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-15 23:57:32 -0600

    Actualizar Literales.py

--- CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/Literales.py
+++ CiscoSkillsForAll/FundamentosPython1/2.1_Literales_Python/Literales.py
@@ -1,29 +1,29 @@
-//Imprime (lo mismo) pero loalmacena de forma distinta 
+#Imprime (lo mismo) pero loalmacena de forma distinta 
 print("2")
 print(2)
-//Numero Octal
+#Numero Octal
 print(0o123)
-//Numero Hexadecimal
+#Numero Hexadecimal
 print(0x123)
 
-//Flotantes
+#Flotantes
 print(3E8)
 
-//Cadenas
-  //Para evitar errores, con caracter de escape tratando de colocar comillas dobles
+#Cadenas
+  #Para evitar errores, con caracter de escape tratando de colocar comillas dobles
 print("Me gusta \"Monty Python\"")
-  //O con apostrofes
+  #O con apostrofes
 print('Me gusta "Monty Python"')
-//Ahora tratando de colocar apostrofes
+#Ahora tratando de colocar apostrofes
   print('I\'m Monty Python.')
 
-//Valores Booleanos
+#Valores Booleanos
 
 print(True > False)
 print(True < False)
 
-//Literales de Python
-  //Uso de caracteres de escape
+#Literales de Python
+  #Uso de caracteres de escape
 print('"Estoy"\n""aprendiendo""\n"""Python"""')
 print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")
  
