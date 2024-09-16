89047fe5c14f14102a1323597ac31dac83736741
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-16 07:07:37 -0600

    Create Introduccion.py

---
+++ SchoolPractices/Simulacion/Introduccion/Introduccion.py
@@ -0,0 +1,11 @@
+#Esta es la introduccion a varias herramienta que vamos a estar viendo a lo largo de esta materia
+#El uso de herraminetas van a ser las siguientes:
+  # Sistema Operativo Linux Ubuntu
+  # Entorno Anaconda Navigator
+  # Python 3.9
+  # Jupyter Notebook
+
+# Bibliotecas
+  # Numpy
+  # Pandas
+  # Matplotlib
