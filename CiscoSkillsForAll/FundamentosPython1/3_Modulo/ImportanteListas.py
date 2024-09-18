b995b49c9ac5377f47121ad337287a23bfe18e3e
JesusSantiago31 <160097941+JesusSantiago31@users.noreply.github.com>
2024-09-17 22:09:02 -0600

    Update ImportanteListas.py

--- CiscoSkillsForAll/FundamentosPython1/3_Modulo/ImportanteListas.py
+++ CiscoSkillsForAll/FundamentosPython1/3_Modulo/ImportanteListas.py
@@ -20,5 +20,14 @@ print(list_2)
 my_list[inicio:fin]
 
 #Ejemplo
+# Copiando la lista completa.
+list_1 = [1]
+list_2 = list_1[:]
+list_1[0] = 2
+print(list_2)
 
+# Copiando parte de la lista.
+my_list = [10, 8, 6, 4, 2]
+new_list = my_list[1:3]
+print(new_list) #Imprime : [1]  [8, 6]
 
