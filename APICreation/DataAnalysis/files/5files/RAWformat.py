ddf19721e68d2823845966081d97adaa7f8272ac
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-11-18 19:53:33 -0600

    2da Prueba

--- APICreation/DataAnalysis/files/5files/RAWformat.py
+++
@@ -1,5 +0,0 @@
-from html.parser import HTMLParser 
-DATASET_RELATIVE_PATH = "/home/jesusssc/Simulacion/files/datasets/datasets/trec07p/data/inmail.1"
-
-inmail = open(DATASET_RELATIVE_PATH).read()
-print(inmail)
--- APICreation/DataAnalysis/files/5files/parsing.py
+++
@@ -1,8 +0,0 @@
-from html.parser import HTMLParser 
-DATASET_RELATIVE_PATH = "/home/jesusssc/Simulacion/files/datasets/datasets/trec07p/data/inmail.1"
-DATASET_FULL_INDEX_PATH = "/home/jesusssc/Simulacion/files/datasets/datasets/trec07p/full/index"
-DATASET_PATH = "/home/jesusssc/Simulacion/files/datasets/datasets/trec07p"
-
-p = Parser()
-p.parse(DATASET_RELATIVE_PATH)
-print(p)
